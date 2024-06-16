1b47286e1e47f115b0f7f1c54228d0c211d68f05
atsushi.yasutake <atsu-yasutake@wearcoord.com>
2024-06-16 14:50:34 +0900

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,20 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import Image from "next/image";
-import { cookies } from "next/headers"
+import { SupabaseClient } from "@supabase/auth-helpers-nextjs";
 import Link from "next/link";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { supabaseServer } from "@/utils/supabaseServer";
 import { Database } from '@/lib/database.types'
 
 // server componentでは引数にcookiesを指定する
-const supabase = createServerComponentClient<Database>({ cookies});
+// const supabase = createServerComponentClient<Database>({ cookies });
 
-const getAllLessons = async () => {
+const getAllLessons = async (supabase: SupabaseClient<Database>) => {
   const { data: lessons } = await supabase.from("lesson").select("*");
   return lessons;
 }
 
 export default async function Home() {
-
-  const lessons = await getAllLessons();
+  const supabase = supabaseServer();
+  const lessons = await getAllLessons(supabase);
 
   return (
     <main className="w-full max-w-3xl mx-auto my-16 px-2">
